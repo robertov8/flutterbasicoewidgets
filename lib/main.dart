a45b37e7d4130c2ba8261711ed99384186fe304d
Roberto Ribeiro <robertov82008@gmail.com>
2019-08-01 09:58:54 -0300

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Flutter'),
+        centerTitle: true,
       ),
-      home: Container(
+      body: Container(
         color: Colors.white,
       ),
     );
   }
 }
-
