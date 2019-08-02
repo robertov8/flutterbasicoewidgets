9ffdae4d257b32aa5b5510964446d0fbe0735442
Roberto Ribeiro <robertov82008@gmail.com>
2019-08-02 11:07:25 -0300

    58 - Stack - Align (parte 2)

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -37,7 +37,13 @@ class HelloListView extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             _img(dog.foto),
-            Text(dog.nome, style: TextStyle(fontSize: 26, color: Colors.white),)
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                dog.nome,
+                style: TextStyle(fontSize: 26, color: Colors.white),
+              ),
+            )
           ],
         );
       },
